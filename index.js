dbefd9551bbc84b0d4f716669781ccb2f4eaa739
amasalov <delsol@bk.ru>
2018-02-18 20:39:11 +0300

    убрал лишнее

--- index.js
+++ index.js
@@ -9,24 +9,24 @@ const description = require(`./src/description`);
 
 const commands = [version, help, author, license, description];
 
-const isCommandExist = (arg) => commands.find((item) => item.name === arg);
+const isCommandExist = arg => commands.find(item => item.name === arg);
 
-const onWrongParam = (param) => {
+const onWrongParam = param => {
   error.execute(param);
   process.exit(1);
 };
 
-const checkUserParam = (arg) => {
+const checkUserParam = arg => {
   if (arg) {
     const command = isCommandExist(arg.replace(`--`, ``));
 
     if (command) {
       command.execute();
-      return;
     } else {
       onWrongParam(arg);
-      return;
     }
+
+    return;
   }
 
   noParams.execute();
