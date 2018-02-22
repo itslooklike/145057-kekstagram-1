a3fcc95c1480e6b529660d835f2d75488888464b
amasalov <delsol@bk.ru>
2018-02-23 00:46:50 +0300

    навигация по папкам, и отдача файлов

--- src/server.js
+++ src/server.js
@@ -1,36 +1,55 @@
 const http = require(`http`);
 const url = require(`url`);
+const path = require(`path`);
 const fs = require(`fs`);
 
 const HOSTNAME = `127.0.0.1`;
 const DEFAULT_PORT = 3000;
+const STATIC_PATH = `/../static`;
+const MIME_MAP = {
+  css: `text/css`,
+  html: `text/html; charset=UTF-8`,
+  jpg: `image/jpeg`,
+  png: `image/png`,
+  ico: `image/x-icon`,
+};
 
-const printDirectory = (files) => `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>Directory content</title>
-    </head>
-    <body>
-      <ul>${files
-      .map((it) => `<li><a href="${it}">${it}</a></li>`)
+const printDirectory = (localPath, files) => {
+  console.log(`localPath`, localPath);
+  const filePath = localPath.replace(
+      path.normalize(__dirname + STATIC_PATH),
+      ``
+  );
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Directory content</title>
+      </head>
+      <body>
+        <ul>${files
+      .map((it) => `<li><a href="${filePath + `/` + it}">${it}</a></li>`)
       .join(``)}</ul>
-    </body>
-  </html>
-`;
+      </body>
+    </html>
+  `;
+};
 
-const readFile = (path, res) => {
-  const data = fs.readFileSync(path);
+const readFile = (localPath, res) => {
+  const data = fs.readFileSync(localPath);
+  const fileExt = path.extname(localPath).replace(`.`, ``);
+  const contentType = MIME_MAP[fileExt];
 
-  res.setHeader(`content-type`, `text/plain`);
+  res.setHeader(`content-type`, contentType);
   res.setHeader(`content-length`, Buffer.byteLength(data));
   res.end(data);
 };
 
-const readDir = (path, res) => {
-  const files = fs.readdirSync(path);
-  const content = printDirectory(files);
+const readDir = (localPath, res) => {
+  const files = fs.readdirSync(localPath);
+  const content = printDirectory(localPath, files);
 
   res.setHeader(`content-type`, `text/html; charset=UTF-8`);
   res.setHeader(`content-length`, Buffer.byteLength(content));
@@ -39,10 +58,17 @@ const readDir = (path, res) => {
 
 const startServer = (port = DEFAULT_PORT) => {
   const server = http.createServer((req, res) => {
-    const staticPath = __dirname + `/../static`;
-    const resPath = staticPath + url.parse(req.url).pathname;
+    const staticRoot = path.normalize(__dirname + STATIC_PATH);
 
-    if (fs.statSync(resPath).isDirectory()) {
+    // обрезаю слеш на конце урла
+    const urlWithoutSlash = url.parse(req.url).pathname.replace(/\/$/, ``);
+
+    const resPath = staticRoot + urlWithoutSlash;
+
+    // if (resPath === `раскоментить, чтобы потестить навигацию по папкам`) {
+    if (resPath === staticRoot) {
+      readFile(resPath + `/index.html`, res);
+    } else if (fs.statSync(resPath).isDirectory()) {
       readDir(resPath, res);
     } else {
       readFile(resPath, res);
